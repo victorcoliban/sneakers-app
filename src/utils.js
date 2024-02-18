b736be160b3bde7f371e10f9aa5e481bb66e901d
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2024-02-18 11:06:05 +0000

    add comment utils file to fix errors

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
-import { useHistory } from 'react-router-dom';
-import { productService } from './services/productService';
+// import { useHistory } from 'react-router-dom';
+// import { productService } from './services/productService';
 
-export function navigateToProduct(product) {
-  this.selectedProducts[product] = true;
-  productService.setCurrentProduct(product);
-  history.push(`/products/${product.name}/${product.colour}`);
-}
+// export function navigateToProduct(product) {
+//   this.selectedProducts[product] = true;
+//   productService.setCurrentProduct(product);
+//   history.push(`/products/${product.name}/${product.colour}`);
+// }
 
-export function getImageUrl(baseImageUrl, width, height) {
-  return `${baseImageUrl}&wid=${width}&hei=${height}`;
-}
+// export function getImageUrl(baseImageUrl, width, height) {
+//   return `${baseImageUrl}&wid=${width}&hei=${height}`;
+// }
